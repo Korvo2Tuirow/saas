14e98ac5b8e4074ddb4d196f5d15dac9d8fbc0a6
Korvo2irow <korvo2irow@live.com>
2025-01-17 17:12:05 -0300

    nav

---
+++ src/components/user-app/user-app-header.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { UserNav } from "../common/user_nav";
+
+export default function UserAppHeader(){
+    return(
+        <header>
+            <nav>
+                <span
+                >re<span>Store</span>
+                </span>
+                <UserNav/>
+            </nav>
+
+        </header>
+    )
+}
